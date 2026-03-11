404d462dc61c3f79705e8d6737f3dbc609622ac7
JJ <jjo920@hotmail.com>
2026-03-11 02:15:16 -0400

    Update

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,7 +11,30 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    {
+      '\\',
+      function()
+        local reveal_file = vim.fn.expand '%:p'
+        if reveal_file == '' then
+          reveal_file = vim.fn.getcwd()
+        else
+          local stat = vim.uv.fs_stat(reveal_file)
+          if not stat then
+            reveal_file = vim.fn.getcwd()
+          end
+        end
+
+        require('neo-tree.command').execute {
+          action = 'focus',
+          source = 'filesystem',
+          position = 'left',
+          reveal_file = reveal_file,
+          reveal_force_cwd = true,
+        }
+      end,
+      desc = 'NeoTree reveal current file or cwd',
+      silent = true,
+    },
   },
   opts = {
     filesystem = {
