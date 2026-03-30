6e1a4013eecb6ce03c0ffc47f56131de4104f1bb
JJ <jjo920@hotmail.com>
2026-03-30 02:49:04 -0400

    Update

--- init.lua
+++ init.lua
@@ -1526,6 +1526,7 @@ require('lazy').setup({
   -- { import = 'custom.plugins' },
 }, {
   ui = {
+    rocks = { hererocks = true },
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
